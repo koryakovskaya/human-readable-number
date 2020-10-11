8d827cfe4eafaef6b239aae681d7ba08859df9c0
Userkoryakovskaya <koryakovskaya.tatiana@gmail.com>
2020-10-11 22:49:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+function toReadable (number) {
+    let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 
+    'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve ','thirteen ',
+    'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+    let tens = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    let great = ['', 'thousand ', 'million ', 'billion ', 'trillion '];
+    let numInWords = '';
+
+    for (let i = 0; i < great.length; i++) {
+        let transNumber = number % (100 * Math.pow(1000, i));
+        if (Math.floor(transNumber /(Math.pow(1000, i))) !== 0) {
+            if (Math.floor(transNumber /(Math.pow(1000, i))) < 20) {
+                numInWords = one[Math.floor(transNumber /(Math.pow(1000, i)))] + great[i] + numInWords;
+            } else {
+                numInWords = tens[Math.floor(transNumber /(10 * Math.pow(1000, i)))] +   
+                one[Math.floor(transNumber /(Math.pow(1000, i))) % 10] + ' ' + great[i] + numInWords;
+            }
+        }
+
+        transNumber = number % (Math.pow(1000, i + 1));
+        if (Math.floor(transNumber / (100 * Math.pow(1000, i))) !== 0) 
+        numInWords =  one[Math.floor(transNumber / (100 * Math.pow(1000,i)))] 
+        + ' ' + 'hundred ' + numInWords;
+    }
+
+    return numInWords;
 }
